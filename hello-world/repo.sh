7638eb57bb76c7361b481f2718435a8e88011d5c
Nogeese <leon8326nogeese@outlook.com>
2025-02-02 17:02:24 +0100

    Create repo.sh

---
+++ hello-world/repo.sh
@@ -0,0 +1,8 @@
+# PASS Repo Metadata
+
+# Basic Info
+PASSREPONAME=hello-world-repo
+
+# Source
+PASSREPOMAINSOURCE=https://raw.githubusercontent.com/leon8326-nogeese/PASS/main/hello-world/
+PASSREPOMETADATASOURCE=https://raw.githubusercontent.com/leon8326-nogeese/PASS/main/hello-world/repo.sh
