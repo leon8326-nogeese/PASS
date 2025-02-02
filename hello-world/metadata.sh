d00aa49b8a4899d65630ca017c059821a944a8ae
Nogeese <leon8326nogeese@outlook.com>
2025-02-02 16:56:08 +0100

    Create metadata.sh

---
+++ hello-world/metadata.sh
@@ -0,0 +1,12 @@
+# PASS METADATA
+
+# Basic Info
+PASSPACKAGENAME=hello-world
+PASSLATESTVERSION=1.00
+
+# Source Settings
+PASSAPKENABLE=false
+PASSCLIENABLE=true
+
+# Source
+PASSCLIINSTALLERSOURCE=https://raw.githubusercontent.com/leon8326-nogeese/PASS/main/hello-world/installer.sh
